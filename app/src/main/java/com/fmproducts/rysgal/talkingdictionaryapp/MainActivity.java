ba6849d5d1b99c152e8295489863fa7d32ca8bad
User <fariz.mamedowtm@gmail.com>
2020-01-01 20:19:51 +0500

    01

---
+++ app/src/main/java/com/fmproducts/rysgal/talkingdictionaryapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fmproducts.rysgal.talkingdictionaryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
